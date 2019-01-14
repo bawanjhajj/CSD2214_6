7b1c4275f29115323fefd49421fc1f2280947986
bawanjhajj <43039582+bawanjhajj@users.noreply.github.com>
2019-01-14 16:52:53 -0500

    Week2-ClassActivity

---
+++ Week2/Script.js
@@ -0,0 +1,21 @@
+// JavaScript source code
+// Name: Bawanpreet Kaur Jhajj
+
+// Student: C0744005
+
+// Course : CSD 2214 Section 6
+
+alert("Yeah!!! I am a GREAT JavaScript Programmer!!");
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
+
+
+
+
+//console.log(endDate.value);
